3e2a9361683a9bfbb3c7165ea0b9a162b196c9a4
FrankHorrigun <FrankHorrigun@mail.ru>
2023-03-23 23:40:23 +0200

    Java start

---
+++ lec_01.java
@@ -0,0 +1,22 @@
+/**
+ * lec_01
+ */
+// public class lec_01 {
+// public static void main(String[] args) {
+// System.out.println("Bye world");
+// }
+
+// }
+
+public class lec_01 {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][5];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.printf("%d ", arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
