36b00841ce20ee94726a5ad5183881cb1b91bb63
FrankHorrigun <FrankHorrigun@mail.ru>
2023-03-31 07:13:44 +0200

    sem1-3

---
+++ Sem_01/Task_03.java
@@ -0,0 +1,19 @@
+package Sem_01;
+
+import java.util.Scanner;
+
+public class Task_03 {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 3, 2, 2, 3, 3 };
+        Scanner iScanner = new Scanner(System.in);
+        System.out.println("Input number: ");
+        int num = Integer.parseInt(iScanner.nextLine());
+        
+        for (int item : res) {
+            System.out.printf("%d", item);
+        }
+
+    }
+}
+
+// задан массив. вводим число, если в массиве есть числа, равные заданному, их нужно перенести в конец массива
