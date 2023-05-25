eceb9b10d4660e5c2c5d5e3d85a9abddb6c07a72
FrankHorrigun <FrankHorrigun@mail.ru>
2023-05-25 21:58:43 +0200

    2sem +++

--- Home_01/Task_03.java
+++ Home_01/Task_03.java
@@ -2,7 +2,7 @@ package Home_01;
 
 import java.util.Scanner;
 
-import javax.lang.model.element.VariableElement;
+
 
 public class Task_03 {
     public static void main(String[] args) {
@@ -11,18 +11,34 @@ public class Task_03 {
         // ’b’)
         System.out.println("Input any key to continue. Input b for quit");// не понимаю почему на пробел не работает
         while (chr() != 'b') {
-            calc();
+            Double x = promt();
+            Double y = promt();
+            Character chr = chr();
+            if (chr != '+' or chr!='-' or chr!='*' or chr!='/') {
+                System.out.println("Wrong operation character");
+                Character chr = chr();
+            }
+            else calc(x, y, chr);
         }
         // calc();
     }
 
-    public static double promt() {
-        Scanner iScanner = new Scanner(System.in);
-        System.out.print("Input number: ");
-        double num = Double.parseDouble(iScanner.nextLine());
-        iScanner.close();
-        return num;
-    }
+    private static int userInput(String message, Scanner sc) {
+        boolean flag = false;
+        int result = 0;
+        while (!flag) {
+            System.out.print(message + ": ");
+            String temp = sc.next();
+            try {
+                result = Integer.parseInt(temp);
+                flag = true;
+            } catch (NumberFormatException ex) {
+                System.out.println("Wrong input, try again!");
+            }
+        }
+        System.out.println("Correct input.");
+        return result;
+    
 
     public static Character chr() {
         Scanner iScanner = new Scanner(System.in);
@@ -33,13 +49,18 @@ public class Task_03 {
     }
 
     public static double calc(double x, double y, Character chr) {
+        
         double res;
         switch (chr) {
-            case '+': res = x + y;
-            case '-': res = x - y;
-            case '*': res = x * y;
-            case '/': res = x / y;
-            // default -> System.out.println("Wrong operation character");
+            case '+':
+                res = x + y;
+            case '-':
+                res = x - y;
+            case '*':
+                res = x * y;
+            case '/':
+                res = x / y;
+                // default -> System.out.println("Wrong operation character");
         }
         return res;
     }
---
+++ Home_02/Task_02.java
@@ -0,0 +1,31 @@
+package Home_02;
+
+import java.util.Scanner;
+
+// Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+public class Task_02 {
+    
+    Scanner iScanner = new Scanner(System.in);
+    System.out.print("Input array length: ");
+    int size = Integer.parseInt(iScanner.nextLine());
+    iScanner.close();
+    Integer[] randArray = fillArray(size);
+    System.out.println(randArray.toString);
+
+
+    public static int[] fillArray(Integer size) {// generator
+        
+        Random rnd = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rnd.nextInt(10);
+        }
+        return arr;
+    }
+
+
+    // public static void bubbleSort(Integer[] randArray) {
+        
+    // }
+
+}
